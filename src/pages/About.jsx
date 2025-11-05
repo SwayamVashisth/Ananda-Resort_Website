353338e3c1f997f79e6e15491c7191063a55505b
Swayam Vashisth <swayamvashisth54.com>
2025-11-05 13:47:32 +0530

    Updated Features /design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
           transition={{ duration: 0.8 }}
           viewport={{ once: true }}
         >
-          <img src="C:\Users\hp\Desktop\resort\ananda\public\images\room.png"
+          <img src="\images\room.png"
             
           />
         </motion.div>
